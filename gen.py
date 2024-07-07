895c9289733a550039e6744782e80cce35c4e363
Moon-404 <zhouzihao2008@126.com>
2024-07-07 14:04:54 +0800

    枪包分开

--- gen.py
+++ gen.py
@@ -37,14 +37,3 @@ for i in range(12):
 #         f.write("""tellraw @a [{"selector": "@a[team=%s]"}, " 是胜利者！排名分：", {"score": {"name": "game_rank", "objective": "global"}}]
 # scoreboard players add @a[team=%s] stat_win 1
 # scoreboard players operation @a[team=%s] rank += game_rank global""" % (team, team, team))
-
-damage = 0.25
-distance = 6.4
-def dmg1(stage):
-    return damage * (4 + stage)
-def dmg2(stage):
-    return damage * (11 - stage)
-for i in range(5, 10):
-    s = '{"distance": %.1f, "damage": %.2f},' % (distance * i, dmg2(i - 4))
-    print(s)
-print('{"distance": "infinite", "damage": %.2f},' % (dmg2(6)))
